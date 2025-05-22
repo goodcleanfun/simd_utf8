23831e0f70e0ab601fd4fcd6422b53b867f98a7b
Al <albarrentine@gmail.com>
2025-05-22 02:00:33 -0400

    [print] making sure pointers are aligend

--- test.c
+++ test.c
@@ -11,12 +11,14 @@
 
 TEST test_utf8_valid(void) {
     const alignas(32) unsigned char *data = (unsigned char *)"we on a world tour نحن في جولة حول العالم nous sommes en tournée mondiale мы в мировом турне a wa lori irin-ajo agbaye 私たちは世界ツアー中です είμαστε σε παγκόσμια περιοδεία በአለም ጉብኝት ላይ ነን jesteśmy w trasie dookoła świata 우리는 세계 여행을 하고 있어요 យើងកំពុងធ្វើដំណើរជុំវិញពិភពលោក ನಾವು ವಿಶ್ವ ಪ್ರವಾಸದಲ್ಲಿದ್ದೇವೆ. մենք համաշխարհային շրջագայության մեջ ենք míele xexeame katã ƒe tsaɖiɖi aɖe dzi เรากำลังทัวร์รอบโลก हम विश्व भ्रमण पर हैं pachantinpi puriypin kashanchis אנחנו בסיבוב הופעות עולמי kaulâh bâdâ è tur dhunnya qegħdin fuq tour tad-dinja ང་ཚོ་འཛམ་གླིང་སྐོར་བསྐྱོད་བྱེད་བཞིན་ཡོད།";
+    printf("data ptr: %p\n", data);
     size_t len = strlen((const char *)data);
     size_t error_index;
     bool valid = utf8_valid(data, len, &error_index);
     ASSERT(valid);
 
     const alignas(32) unsigned char *invalid = (unsigned char *)"abcdefghijklmnopqrstuvwxyzabcd\x80\x01";
+    printf("invalid ptr: %p\n", invalid);
     len = strlen((const char *)invalid);
     valid = utf8_valid(invalid, len, &error_index);
     ASSERT(!valid);
